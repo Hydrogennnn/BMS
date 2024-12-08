bb7db3c6a9b5fba82989aa465dd652966f7c42a3
Wangsyuu <1079229157@qq.com>
2024-12-08 14:18:28 +0800

    News.vue

---
+++ vue/src/views/News.vue
@@ -0,0 +1,107 @@
+<template>
+  <div>
+    <el-card style="width: 80%; margin: 40px auto;">
+      <h2 style="padding: 30px">资源动态</h2>
+      <el-table :data="newsList" style="width: 100%">
+        <el-table-column prop="title" label="标题" width="800">
+          <template #default="{ row }">
+            <el-link @click="showDetail(row)">{{ row.title }}</el-link>
+          </template>
+        </el-table-column>
+        <el-table-column prop="time" label="时间" width="340">
+          <template #default="{ row }">
+            {{ formatDate(row.time) }}
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+
+
+    <!-- 显示详情 -->
+    <el-card v-if="selectedNews" style="width: 80%; margin: 40px auto;">
+      <h3 class="detail-title">{{ selectedNews.title }}</h3>
+      <p class="detail-time">{{ formatDate(selectedNews.time) }}</p>
+      <p class="detail-content">{{ selectedNews.content }}</p>
+      <div class="button-container">
+        <el-button @click="closeDetail">关闭</el-button>
+      </div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
+import request from '@/utils/request';
+import { ElMessage } from 'element-plus';
+
+export default {
+  name: "LatestNews",
+  setup() {
+    const newsList = ref([]);
+    const selectedNews = ref(null);
+
+    // 获取最新新闻
+    const fetchNews = async () => {
+      try {
+        const response = await request.get('/news');
+        newsList.value = response.data.records;
+      } catch (error) {
+        console.error('获取最新资讯失败:', error);
+        ElMessage.error('获取最新资讯失败，请稍后重试。');
+      }
+    };
+
+    // 显示详情
+    const showDetail = (news) => {
+      selectedNews.value = news;
+    };
+
+    // 关闭详情
+    const closeDetail = () => {
+      selectedNews.value = null;
+    };
+
+    // 格式化日期
+    const formatDate = (date) => {
+      return new Intl.DateTimeFormat('zh-CN', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      }).format(new Date(date));
+    };
+
+    onMounted(fetchNews);
+    return {
+      newsList,
+      selectedNews,
+      formatDate,
+      showDetail,
+      closeDetail
+    };
+  }
+}
+</script>
+
+<style scoped>
+.detail-title {
+  text-align: center;
+  margin: 0;
+}
+
+.detail-time {
+  text-align: center;
+  margin: 10px 0;
+}
+
+.detail-content {
+  margin-top: 20px;
+}
+
+.button-container {
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
